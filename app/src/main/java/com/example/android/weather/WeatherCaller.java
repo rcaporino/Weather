cad83bb7e12bf0f16506952d708c5ea71654e115
John Baker <jbaker@student.sjcny.edu>
2017-02-27 20:54:11 -0500

    Put in WeatherCaller.java only

--- app/src/main/java/com/example/android/weather/WeatherCaller.java
+++ app/src/main/java/com/example/android/weather/WeatherCaller.java
@@ -11,7 +11,7 @@ import java.net.URL;
  */
 
 public class WeatherCaller {
-private String url = "";
+private String url = "http://api.openweathermap.org/data/2.5/forecast?q=Patchogue,us&cnt=7&appid=c3368eff18484472b806c8fbdf3df950";
 
     public WeatherCaller(String newURL){
         url = newURL;
